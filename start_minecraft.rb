8bc3c0827ccad23e1c97b03e2c5c3deb9be8a867
Alex Clink <code@alexclink.com>
2014-07-07 00:18:54 -0400

    added a delay time parameter to the stop_minecraft script. Also updated the cron jobs

--- start_minecraft.rb
+++ start_minecraft.rb
@@ -12,6 +12,11 @@ server = Minecraft.new
 cron = Crontab.new
 
 server.start
-unless cron.add_job("0 */3 * * * ruby /home/minecraft/server/scripts/backup_minecraft.rb")
-  puts "Cron was already loaded."
+
+unless cron.add_job("* */3 * * * ruby /home/minecraft/server/scripts/backup_minecraft.rb 2>&1 >> /home/minecraft/backup.log")
+  puts "Backup cron job was already loaded."
+end
+
+unless cron.add_job("* */1 * * * ruby /home/minecraft/server/scripts/update_player_db.rb 2>&1 >> /home/minecraft/player_db.log")
+  puts "Update players cron job was already loaded"
 end
--- stop_minecraft.rb
+++ stop_minecraft.rb
@@ -3,19 +3,23 @@
 require "~/server/scripts/Crontab.rb"
 require "~/server/scripts/Minecraft.rb"
 
-# server/scripts
-Dir.chdir(File.expand_path(File.dirname(__FILE__)))
-# server
-Dir.chdir("..")
+stop_time = ARGV[0] || 30 # usage stop_minecraft.rb [delay]
+
+Dir.chdir(File.expand_path(File.dirname(__FILE__))) # server/scripts
+Dir.chdir("..") # server
 
 server = Minecraft.new
 cron = Crontab.new
 
+puts "Stopping server"
+server.say("Stopping server in #{stop_time} seconds")
 
-server.say("Stopping server in 30 seconds")
-`sleep 20`
+while stop_time > 10 do
+  stop_time -= 1;
+  `sleep 1`
+end
 
-10.downto(0) do |time|
+stop_time.downto(0) do |time|
   server.say("Stopping server in #{time} seconds")
   `sleep 1`
 end
@@ -23,4 +27,5 @@ end
 server.say("Goodbye")
 `sleep 1`
 server.stop
-cron.remove_job("0 */3 * * * ruby /home/minecraft/server/scripts/backup_minecraft.rb")
+cron.remove_job("* */3 * * * ruby /home/minecraft/server/scripts/backup_minecraft.rb 2>&1 >> /home/minecraft/backup.log")
+cron.remove_job("* */1 * * * ruby /home/minecraft/server/scripts/update_player_db.rb 2>&1 >> /home/minecraft/player_db.log")
