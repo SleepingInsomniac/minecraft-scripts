8946e381571d295829fe1973d5d083a674743a66
Alex Clink <code@alexclink.com>
2014-07-14 02:25:42 -0400

    Updates to the player commands

--- Crontab.rb
+++ Crontab.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Crontab
   
   def get_jobs
--- check_log.rb
+++ check_log.rb
@@ -13,10 +13,32 @@ server = Minecraft.new
 while true do
   if log.changed?
     log.since_last.each do |l|
-      if player = l.parse[:msg].match(/(\S+) joined the game$/i)
+      line = l.parse
+      # server.say "#{line}"
+      if player = line[:msg].match(/(\S+) joined the game$/i)
         server.whisper player[1], "Welcome to Pixelfaucet #{player[1]}!"
         server.whisper player[1], "http://minecraft.pixelfaucet.com"
+        server.whisper player[1], "For info/rules type !info or !rules"
         puts "Greeting player #{player[1]}"
+      end
+      if command = line[:msg].match(/^\<(\S+)\>\s*\!([a-z]+)$/i)
+        p = command[1]
+        c = command[2].downcase
+        server.say "#{p}'s Command was #{command[2]}"
+        if p == "SlpingInsomniac"
+          case c
+          when "backup"
+            `./../scripts/backup_minecraft.rb`
+          end
+        end
+        case c
+        when "info"
+          server.say "I know the the following commands:"
+          server.say "!info - this message"
+          server.say "!web - website"
+        when c.match(/^(web|site|website)$/)
+          server.say "Pixelfaucet.com"
+        end
       end
     end
     log.register_change
